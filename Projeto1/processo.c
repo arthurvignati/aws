52360d2e18a579e909ca2fef27ca94441abc59fd
arthurvignati <161461962+arthurvignati@users.noreply.github.com>
2024-04-16 15:11:44 -0300

    Update processo.c

--- Projeto1/processo.c
+++ Projeto1/processo.c
@@ -3,43 +3,58 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-int main(void) {
-  pid_t pid;
+typedef struct {
+    int n;
+    int* t;
+    int* direcoes;
+} Passageiros;
 
-  int n;
-  printf("Quantas pessoas? R: ")
-  scanf("%d", &n);
+Passageiros lerPassageiros(const char* caminhoArquivo) {
+  FILE* arquivo = fopen(caminhoArquivo, "r");
+  if (arquivo == NULL) {
+    fprintf(stderr, "Erro ao abrir o arquivo %s.\n", caminhoArquivo);
+    exit(EXIT_FAILURE);
+  }  
+  Passageiros p; 
+  fscanf(arquivo, "%d", &p.n);
+  p.t = (int*) malloc(p.n * sizeof(int));
+  p.direcoes = (int*) malloc(p.n * sizeof(int));
 
-  int t[n], direcoes[n];
+  for (int i = 0; i < p.n; i++) {
+    fscanf(arquivo, "%d %d", &p.t[i], &p.direcoes[i]);
+  }
+  fclose(arquivo);
+  retun p;  
+}
+
+int main() {
+  Passageiros p = lerPassageiros("./input/E_4");  
+  
   int fim_subida = 0;
   int fim_descida = 0;
   int tempo_final = 0;
-
-  for (int i=0; i<n; i++){
-    printf("Digite o momento e a direção para a pessoa %d: ", i+1);
-    scanf("%d %d", &t[i], &direcoes[i]);
-  }
-
   int pipefd_filho_pai[2], pipefd_pai_filho[2];
+  
   if (pipe(pipefd_filho_pai) == -1 || pipe(pipefd_pai_filho) == -1){
     perror("Falha ao criar pipe");
     exit(EXIT_FAILURE);
   }
 
-  pid = fork();
+  pid_t pid = fork();
   if (pid < 0) {
     perror("Fork Failed");
-    return 1;
+    exit(EXIT_FAILURE);
   }
   
   //PROCESSO FILHO
   if (pid == 0) {  
     close(pipefd_filho_pai[1]); //fecha escrita filho --> pai
     close(pipefd_pai_filho[0]); // fecha leitura pai --> filho
-    for (int i = 0; i < n; i++){
-      if(direcoes[i] == 0){ //SUBIDA
-        if (t[i] >= fim_descida){
-          fim_subida = t[i] + 10;
+    
+    for (int i = 0; i < p.n; i++){
+      if(p.direcoes[i] == 0){ //SUBIDA
+        if (p.t[i] >= fim_descida){
+          fim_subida = p.t[i] + 10;
         }else{
           fim_subida = fim_descida + 10; 
         }
@@ -48,24 +63,19 @@ int main(void) {
 
     write(pipefd_pai_filho[1], &fim_subida, sizeof(fim_subida));
     close(pipefd_pai_filho[1]);
-
-    // Aguarda a última descida do pai
-    read(pipefd_filho_pai[0], &fim_descida, sizeof(fim_descida));
-    close(pipefd_filho_pai[0]);
-    exit(EXIT_SUCESS)
-    
-  } 
-  //PROCESSO PAI
-  else {  
+    exit(EXIT_SUCESS);    
+   
+    //PROCESSO PAI
+  } else {  
     close(pipefd_filho_pai[0]); //fecha leitura filho --> pai 
     close(pipefd_filho_pai[1]); //fecha escrita pai --> filho
     
     read(pipefd_pai_filho[0], &fim_subida, sizeof(fim_subida));
 
-    for (int i = 0; i < n; i++){  
-      if (direcoes[i] == 1){ //DESCIDA
-        if (t[i] >= fim_subida){
-          fim_descida = t[i] + 10;
+    for (int i = 0; i < p.n; i++){  
+      if (p.direcoes[i] == 1){ //DESCIDA
+        if (p.t[i] >= fim_subida){
+          fim_descida = p.t[i] + 10;
         }
         else{
           fim_descida = fim_subida + 10;
@@ -76,12 +86,12 @@ int main(void) {
     write(pipefd_filho_pai[1], &fim_descida, sizeof(fim_descida));
     close(pipefd_filho_pai[1]);
     wait(NULL); // Espera pelo término do filho
-
+    close(pipefd_pai_filho[0]);
     
     tempo_final = (fim_subida > fim_descida) ? fim_subida : fim_descida;
-    printf("Tempo final: %d\n", tempo_final);
-    close(pipefd_pai_filho[0]);
-        
+    printf("Tempo final: %d\n", tempo_final);        
     }
+  free(p.t);
+  free(direcoes.t);
   return 0;
 }
