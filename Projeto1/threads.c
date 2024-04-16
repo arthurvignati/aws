81ae8380076d2006acc54c1e109f3c379fba4832
arthurvignati <161461962+arthurvignati@users.noreply.github.com>
2024-04-16 08:27:50 -0300

    Update threads.c

--- Projeto1/threads.c
+++ Projeto1/threads.c
@@ -14,5 +14,31 @@ pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t pode_descer = PTHREAD_COND_INITIALIZER;
 pthread_cond_t pode_subir = PTHREAD_COND_INITIALIZER;
 
+EscadaArgs lerPassageiros(char* caminhoArquivo) {
+    FILE* arquivo = fopen(caminhoArquivo, "r");
+    if (arquivo == NULL) {
+        printf("Erro ao abrir o arquivo %s.\n", caminhoArquivo);
+        exit(1);
+    } else {
+      printf("Arquivo %s aberto com sucesso.\n", caminhoArquivo);
+  }
+
+    EscadaArgs escadaArgs;
+
+    fscanf(arquivo, "%d", &escadaArgs.n);
+
+    escadaArgs.t = (int*)malloc(escadaArgs.n * sizeof(int));
+    escadaArgs.direcoes = (int*)malloc(escadaArgs.n * sizeof(int));
+
+    for (int i = 0; i < escadaArgs.n; i++) {
+        fscanf(arquivo, "%d %d", &escadaArgs.t[i], &escadaArgs.direcoes[i]);
+    }
+
+    fclose(arquivo);
+
+    return escadaArgs;
+}
+
+
 
 
