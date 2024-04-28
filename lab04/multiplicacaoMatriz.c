5fe14e8abb16f3f15acb6fe35e722466389a3baf
arthurvignati <161461962+arthurvignati@users.noreply.github.com>
2024-04-28 09:06:59 -0300

    Update multiplicacaoMatriz.c

--- lab04/multiplicacaoMatriz.c
+++ lab04/multiplicacaoMatriz.c
@@ -8,3 +8,15 @@ double x[3] = {1, 3, 4};
 double y[4]; 
 int thread_count = 4;
 
+void Pth_matriz_vetor(void *rank) {
+  long my_rank = (long)rank;
+  int i, j;
+  int local_m = m / thread_count;
+  
+  int start_row = my_rank * local_m;
+  int end_row = (my_rank == thread_count - 1) ? m - 1 : start_row + local_m - 1;
+
+  for (int i = start_row; i <= end_row; i++){
+    
+  }
+}
