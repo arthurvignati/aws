0b6fe9fa0a433bd7d0897c8cf56f14d3d7f7dbc7
arthurvignati <161461962+arthurvignati@users.noreply.github.com>
2024-04-28 09:06:15 -0300

    Update multiplicacaoMatriz.c

--- lab04/multiplicacaoMatriz.c
+++ lab04/multiplicacaoMatriz.c
@@ -1,1 +1,10 @@
+#include <pthread.h>
+#include <stdio.h>
+
+int m = 4;
+int n = 3;
+double A[4][3] = {{2, 2, 3}, {4, 3, 6}, {4, 4, 9}, {5, 11, 6}};
+double x[3] = {1, 3, 4};
+double y[4]; 
+int thread_count = 4;
 
