1b1555c558b331e273a03f47ba7001e05a8234a0
arthurvignati <161461962+arthurvignati@users.noreply.github.com>
2024-05-15 15:25:51 -0300

    Update proj2.c

--- Projeto2/proj2.c
+++ Projeto2/proj2.c
@@ -54,3 +54,67 @@ void trocarContas(char *direcao, char *fromSaida, char *toSaida){
   snprintf(fromSaida, 10, "to");
   snprintf(toSaida, 10, "from");
 }
+
+int main(){
+  int i;
+
+  //inicializar mutex
+  if (pthread_mutex_init(&lock, NULL) != 0){
+    perror("Falha mutex_init");
+    exit(1);
+  }
+  
+  from.saldo = 100;
+  to.saldo = 100;
+
+  printf("Transferindo 10 (10 vezes) para a conta c2\n");
+  valor = 10;
+  int transferCount = 0;
+  char direcao[12] = "From → To";
+  char fromSaida[10] = "from";
+  char toSaida[10] = "to";
+
+  for (i = 0; i < 100; i++){
+    struct transfer *args = malloc(sizeof(struct transfer));
+    if (args == NULL){
+      perror("Não foi possível alocar memória");
+      exit(1);
+    }
+    args->from = &from;
+    args->to = &to;
+    args->valor = valor;
+    snprintf(args->direcao, sizeof(args->direcao), "%s", direcao);
+    snprintf(args->fromSaida, sizeof(args->fromSaida), "%s", fromSaida);
+    snprintf(args->toSaida, sizeof(args->toSaida), "%s", toSaida);
+
+    pthread_t thread;
+    if (pthread_create(&thread, NULL, transferencia, (void*)args) != 0){
+      perror("pthread_create");
+      exit(2);
+    }
+    pthread_join(thread, NULL);
+    transferCount++;
+  }
+  if (transferCount % 10 == 0) {
+    printf("Transferências concluídas!\n");
+    printf("Saldo de c1(%s): %d\n", fromSaida, from.saldo);
+    printf("Saldo de c2(%s): %d\n", toSaida, to.saldo);
+    if (from.saldo == 0 || to.saldo == 0){
+      trocarContas(direcao, fromSaida, toSaida);
+      printf("---------------------------------------------------\n");
+      printf("Contas Trocadas. Transferindo 10 (10 vezes) %s\n", direcao);
+    }
+  }
+
+  if (from.saldo == 100 && to.saldo == 100){
+    break;
+  }
+  pthread_mutex_destroy(&lock);
+
+  printf("Transferências concluídas!\n");
+  printf("Saldo de c1: %d\n", from.saldo);
+  printf("Saldo de c2: %d\n", to.saldo);
+
+  return 0;
+  
+}
