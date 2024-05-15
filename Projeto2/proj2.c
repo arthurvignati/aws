933ed380ef87410c8c70e7e681eba38f5b30a9e7
arthurvignati <161461962+arthurvignati@users.noreply.github.com>
2024-05-15 15:16:38 -0300

    Update proj2.c

--- Projeto2/proj2.c
+++ Projeto2/proj2.c
@@ -7,3 +7,23 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+
+#define FIBER_STACK 1024 * 64
+
+struct c{
+int saldo;
+};
+
+typedef struct c conta;
+conta from, to;
+int valor;
+pthread_mutex_t lock; //Declara um mutex
+
+struct transfer{
+conta *from;
+conta *to;
+int valor;
+char direcao[12];
+char fromSaida[10];
+char toSaida[10];
+};
